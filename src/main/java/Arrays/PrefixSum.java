f64cf77f7a0824228e4828d2a429e9b58e0a418e
cyberhoax <raunaktamang@gmail.com>
2022-05-26 23:51:33 +0530

    first commit

---
+++ src/main/java/Arrays/PrefixSum.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class PrefixSum {
+  public static void main(String[] args) {
+    int[] A = {1, 2, 3, 4, 5};
+    int sum = 0;
+    int[] ans = new int[A.length];
+    for (int i = 0; i < A.length; i++) {
+      sum += A[i];
+      ans[i] = sum;
+    }
+    System.out.println(Arrays.toString(ans));
+  }
+}
